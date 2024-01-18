84f94dd5447abfa6dd29f0edfffd53d8a083e719
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-18 16:40:42 +0100

    showing photos on profile

---
+++ src/components/imageGallery.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps } from 'vue';
+
+
+const props = defineProps(['posts'])
+</script>
+
+<template>
+    <div class="image-gallery-container">
+        <img 
+        v-for="post in props.posts" 
+        :key="post.id" 
+        :src="post.image" 
+        alt=""
+        >
+    </div>
+</template>
+
+<style scoped>
+    .image-gallery-container {
+        display: flex;
+        justify-content: left;
+        flex-wrap: wrap;
+    }
+
+    img {
+        margin: 5px;
+        width: 100px;
+    }
+
+</style>
