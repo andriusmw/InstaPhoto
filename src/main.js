d95fc1ce5e6278b838b3b5fb290238b3d12ab5a3
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-18 11:47:55 +0100

    antd design

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Antd from "ant-design-vue"
 import App from './App.vue'
 import router from './router'
+import 'ant-design-vue'
+// in order to make the ant-design to work
+import './assets/main.css'
 
 const app = createApp(App)
 
+
 app.use(createPinia())
+app.use(Antd)
 app.use(router)
 
-app.mount('#app')
+app.mount('#app') 
+
