6d912d19b550b310715d51968f925eafeedbce81
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-19 11:15:20 +0100

    emptying states after validation

--- src/stores/users.js
+++ src/stores/users.js
@@ -32,7 +32,7 @@ export const useUserStore = defineStore('users', () => {
       return errorMessage.value = "You must provide a valid email address"
     }
 
-
+    errorMessage.value = ""
 
   }
 
