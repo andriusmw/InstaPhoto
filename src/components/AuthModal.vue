0015591234586663f60b34dd028b21cee55934f7
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-19 10:48:09 +0100

    linked modal to usersStore

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,15 +1,33 @@
 <script setup>
-import {ref, defineProps} from "vue";
+import {ref, defineProps, reactive} from "vue";
+
+//import the userStore from Pinia (hook)
+import {useUserStore} from "../stores/users"
+//declare a const userStore to use all the functions inside the hook useUserStore()
+const userStore = useUserStore()
+// de-structuring errorMessage and handleSingup from the hook to use them
+const {errorMessage, handleSingup} = userStore
+
 
 const props = defineProps(['isLogin'])
 const visible = ref(false);
+
+//States to save the modal data, reactive is like watch()
+//it is linked by v-model to this object so it updates each part of it
+const userCredentials = reactive({
+    email: "",
+    password: "",
+    username: ""
+
+})
 
 const showModal = () => {
     visible.value = true;
 };
 
+//this is called when we click the "ok" button
 const handleOk = (e) => {
-    visible.value = false;
+   handleSingup(userCredentials)
 };
 
 const title = props.isLogin ? 'Login' : 'Sign Up' 
@@ -19,9 +37,9 @@ const title = props.isLogin ? 'Login' : 'Sign Up'
     <div>
         <a-button type="primary" @click="showModal" class="btn">{{title}}</a-button>
         <a-modal v-model:visible="visible" :title="title" @ok="handleOk">
-           <a-input- class="input" v-if="!isLogin" v-model:value="value" placeholder="Username"/>
-           <a-input  class="input" v-model:value="value" placeholder="Email"></a-input>
-           <a-input  class="input" v-model:value="value" placeholder="Password"></a-input>
+           <a-input- class="input" v-if="!isLogin" v-model:value="userCredentials.username" placeholder="Username"/>
+           <a-input  class="input" v-model:value="userCredentials.email" placeholder="Email"></a-input>
+           <a-input  class="input" v-model:value="userCredentials.password" placeholder="Password"></a-input>
         </a-modal>
     </div>
     
--- src/stores/users.js
+++ src/stores/users.js
@@ -3,14 +3,49 @@ import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('users', () => {
   const user = ref(null);
+  const errorMessage = ref("")
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const handleLogin = () => {}
 
-  const handleSingup = () => {}
+  const handleSingup = (credentials) => {
+    const {email, password, username} = credentials;
+
+    //First validation step
+    if(password.length < 6) {
+      //console.log("password too short")
+      return errorMessage.value = "Password must have 6 or more characters"
+    }
+
+    if(username.length < 4) {
+      return errorMessage.value = "Username must have 4 or more characters"
+    }
+
+    if(!validateEmail(email)){
+      return errorMessage.value = "You must provide a valid email address"
+    }
+
+
+
+  }
+
+   
 
   const handleLogout = () => {}
 
   const getUser = () => {}
 
-  return { user, handleLogin, handleSingup, handleLogout, getUser}
+  return { user, 
+    errorMessage,
+      handleLogin, 
+      handleSingup,
+       handleLogout, 
+       getUser}
 })
