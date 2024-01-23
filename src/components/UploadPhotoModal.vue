786f7d426ecc9e3ea2fb21ae1f1c01c9ee8406cc
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-23 08:35:15 +0100

    after creating handleuploadchange

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -7,6 +7,7 @@
 // ----------------------------CONSTS ------------------------
 // ---------------------------------------------------------- 
     const visible = ref(false);
+    const caption = ref("");
 
 // --------------------------- FUNCTIONS -------------------
 // ---------------------------------------------------------
@@ -20,6 +21,15 @@
         visible.value = false;
     };
 
+    const handleUploadChange = (e) => {
+        // when you upload something it is placed in an array, at possition 0
+        //so  we get the data of the upload
+        if(e.target.files[0]){
+            file.value = e.target.files[0]
+        }
+    }
+
+
 </script>
 
 <!-- -----------------------------TEMPLATE --------------------------
@@ -28,13 +38,31 @@
 <template>
     <div>
         <a-button @click="showModal">Upload Photo</a-button>
-        <a-modal v-model:visible="visible" title="Basic Model" @ok="handleOk">
-            <p>Some contents</p>
-            <p>Some contents</p>
-            <p>Some contents</p>
-            <p>Some contents</p>
+        <a-modal v-model:visible="visible" title="Upload Photo" @ok="handleOk">
+           <input type="file" accept=".jpeg,.png,.jpg" 
+                @change="handleUploadChange"
+           >
+
+           <a-input 
+            v-model:value="caption"  
+            placeholder="Caption..."
+            :maxLength="50"
+            >
+            </a-input>
+
         </a-modal>
     </div>
 
 </template>
+
+<!-- ----------------------------- STYLES -----------------------------------
+----------------------------------------------------------------------------- -->
+
+<style scoped>
+
+    input {
+        margin-top: 10px;
+    }
+
+</style>
 
