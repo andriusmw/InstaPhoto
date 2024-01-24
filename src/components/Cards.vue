fdd51402ebe5c669ee0a7e6e61a5b4482aed6a02
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-24 08:45:54 +0100

    fetching an array of posts of ppl we follow

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -37,8 +37,16 @@ const data = [
 
 const fetchData = async () => {
     // this gets the ids of the people we are following
-    const response = await supabase.from("followers_following")
+    const {data: followings} = await supabase.from("followers_following")
         .select("following_id").eq("follower_id", user.value.id)
+
+    // turns the array of objects that is followings into an array of numbers 
+    //where each number is a following_id
+    const owner_ids = followings.map(f => f.following_id)    
+
+    //gets all the posts from those owner_ids (which are the ones we are folling)
+    const res = await supabase.from("posts")
+        .select().in('owner_id', [owner_ids])  
 }
 
 //-------------------------------------------------
