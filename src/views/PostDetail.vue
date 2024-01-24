4a465fa528a2469ec7d3f1b94785a66a9286d81b
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-24 16:41:36 +0100

    delete working

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,11 +1,48 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted} from 'vue';
 import { supabase } from '../../supabase';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
+import {useUserStore} from "../stores/users"
+import { storeToRefs } from "pinia";
 
+// ------------------------------------ CONSTS -------------------------------------------
+// --------------------------------------------------------------------------------------
+const userStore = useUserStore()
+const {user: loggedInUser} = storeToRefs(userStore)
 const route = useRoute();
+const router = useRouter();
 const post = ref(null);
 const {VITE_BASE_PHOTO_URL} = import.meta.env
+const isOwner = ref(false)
+
+// Comprobar si el owner_id del post es igual al id del usuario actual
+const CheckOwner = (() => {
+    if(post){
+        if (post.value.owner_id === loggedInUser.value.id) {
+            isOwner.value = true
+              console.log("isowner TRUE")
+        } else {
+             isOwner.value = false
+             console.log("isowner FALSE")
+        }
+    } else {
+        console.log("No hay post")
+    }
+   
+});
+
+// Función para borrar el post
+const deletePost = async () => {
+  if (post.value) {
+    // Realizar la lógica para borrar el post en Supabase
+    await supabase.from('posts').delete().eq('id', post.value.id);
+
+    // Redirigir a una página de inicio o a donde desees después de borrar el post
+    router.push('/');
+  }
+};
+
+//----------------------------------
 
 onMounted(async () => {
     // Obtaining the postId from the rpute params
@@ -20,6 +57,10 @@ onMounted(async () => {
   // sending the  details of the post to post.value
   post.value = postData;
   //console.log(postData)
+
+    //Calls the function to check the owner
+    CheckOwner()
+
 });
 </script>
 
@@ -31,6 +72,8 @@ onMounted(async () => {
       <img :src="`${VITE_BASE_PHOTO_URL}${post.url}`" alt="Post Image" />
       <p>{{ post.username }}</p>
         <p>{{post.caption}}</p>
+        <!-- Mostrar el botón de borrar solo si el owner_id es igual al id del usuario actual -->
+      <button v-if="isOwner" @click="deletePost">Borrar Post</button>
     </div>
     <div v-else>
       <p>Loading details...</p>
